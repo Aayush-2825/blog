3c7b3ff6aec73ec2e78a7bf6ce45d2232cf3fba2
Aayush Raj <aayush.ardy@gmail.com>
2025-04-14 17:18:16 +0530

    refactor: update ESLint config to disable unused vars check and allow require() imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,10 +12,11 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 
-  // ✅ Custom override to allow require()
+  // Disable unused vars globally
   {
     rules: {
-      "@typescript-eslint/no-require-imports": "off",
+      '@typescript-eslint/no-unused-vars': 'off',  // Disable unused vars check globally
+      '@typescript-eslint/no-require-imports': 'off',  // Optional, disable require() imports check
     },
   },
 ];
